f57e37ab4b0752a5c44e21086b450f104f5bb4ff
AlenaPliusnina <64252655+AlenaPliusnina@users.noreply.github.com>
2020-08-24 10:17:20 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="trello_client-basics-api-alenapliusnina", version="0.0.1", author="Alena Pliusnina",
+    author_email="a564352@gmail.com",
+    description="Trello console client", long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="[https://github.com/AlenaPliusnina/Trello-client](https://github.com/AlenaPliusnina/Trello-client)",
+    # packages=setuptools.find_packages(),
+
+    # classifiers=[ "Programming Language :: Python :: 3", "License :: OSI Approved :: MIT License",
+    # "Operating System :: OS Independent", ],
+
+    # python_requires='>=3.6',
+)
+
+
