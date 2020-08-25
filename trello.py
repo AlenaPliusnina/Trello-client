cdeeb5999950ebd96e9f22945cbb821a3cf8c0b5
AlenaPliusnina <64252655+AlenaPliusnina@users.noreply.github.com>
2020-08-25 14:59:40 +0300

    add 3 new funcs and update create func

--- trello.py
+++ trello.py
@@ -15,6 +15,8 @@ board_id = "___"
 
 
 def read():
+    # Обновляем счетсик задач в названии колонок
+    add_counter()
     # Получим данные всех колонок на доске:
     column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
 
@@ -23,6 +25,7 @@ def read():
         print(column['name'])
         # Получим данные всех задач в колонке и перечислим все названия
         task_data = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()
+
         if not task_data:
             print('\t' + 'Нет задач!')
             continue
@@ -36,7 +39,7 @@ def create(name, column_name):
 
     # Переберём данные обо всех колонках, пока не найдём ту колонку, которая нам нужна
     for column in column_data:
-        if column['name'] == column_name:
+        if get_column_name(column['name']) == column_name:
             # Создадим задачу с именем _name_ в найденной колонке
             requests.post(base_url.format('cards'), data={'name': name, 'idList': column['id'], **auth_params})
             break
@@ -47,24 +50,78 @@ def move(name, column_name):
     column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
 
     # Среди всех колонок нужно найти задачу по имени и получить её id
-    task_id = None
+    tasks = []
     for column in column_data:
         column_tasks = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()
         for task in column_tasks:
             if task['name'] == name:
-                task_id = task['id']
-                break
-        if task_id:
-            break
+                tasks.append({'id': task['id'], 'idShort': task['idShort'], 'idList': task['idList'], 'column': get_column_name(column['name'])})
+
+    if len(tasks) > 1:
+        print("Найдено несколько задач с именем \"{}\":".format(name))
+        for i, task in enumerate(tasks):
+            print("Порядковый номер: {}. Идентификатор - {}, расположена в колонке {}.".format(i, task['id'], task['column']))
+
+        task_index = int(input("Введите порядковый номер задачи которую хотите перенсти в колонку \"{}\": ".format(column_name)))
+        while int(task_index) > len(tasks) or int(task_index) < 0:
+            task_index = int(input("Вы ввели не корректный номер задачи. Попробуйте еще раз: "))
+
+        task_id = tasks[task_index]['id']
+    else:
+        task_id = tasks[0]['id']
 
     # Теперь, когда у нас есть id задачи, которую мы хотим переместить
     # Переберём данные обо всех колонках, пока не найдём ту, в которую мы будем перемещать задачу
     for column in column_data:
-        if column['name'] == column_name:
+        if get_column_name(column['name']) == column_name:
             # И выполним запрос к API для перемещения задачи в нужную колонку
             requests.put(base_url.format('cards') + '/' + task_id + '/idList',
                          data={'value': column['id'], **auth_params})
             break
+
+
+# Функция добавления счетчика задач в название колонки
+def add_counter():
+    counter = 0
+    # Получим данные всех колонок на доске
+    column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
+
+    for column in column_data:
+        column_tasks = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()
+        # Подсчитываем колличество задач в колонке
+        for task in column_tasks:
+            counter += 1
+
+        # Обновляем данные с названием колонки
+        if column['name'].isalpha():
+            requests.put(base_url.format('lists') + '/' + column['id'],
+                         data={'name': column['name'] + " " + str(counter), **auth_params})
+        else:
+            split_name = column['name'].split(" ")
+            new_name = ""
+            for item in split_name:
+                if item.isalpha():
+                    new_name = new_name + " " + item
+
+            requests.put(base_url.format('lists') + '/' + column['id'],
+                         data={'name': new_name[1:] + " " + str(counter), **auth_params})
+
+        counter = 0
+
+
+# Функция возвращает название колонки без счетчика задач
+def get_column_name(name):
+    new_name = ""
+    split_name = name.split(" ")
+    for item in split_name:
+        if item.isalpha():
+            new_name = new_name + " " + item
+    return new_name[1:]
+
+
+# Функция добавления новой колонки 
+def add_column(name):
+    requests.post(base_url.format('boards') + '/' + board_id + '/lists', params={'name': name, **auth_params})
 
 
 if __name__ == "__main__":
@@ -74,3 +131,5 @@ if __name__ == "__main__":
         create(sys.argv[2], sys.argv[3])
     elif sys.argv[1] == 'move':
         move(sys.argv[2], sys.argv[3])
+    elif sys.argv[1] == 'add_column':
+        add_column(sys.argv[2])
